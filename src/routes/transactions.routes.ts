131b4ef5d134c8e627eca854aba4d12707ddf4fc
Gstv <gstv.almeida09@gmail.com>
2020-10-21 22:07:10 -0300

    first commit

---
+++ src/routes/transactions.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+const transactionsRouter = Router();
+
+transactionsRouter.get('/', async (request, response) => {
+  // TODO
+});
+
+transactionsRouter.post('/', async (request, response) => {
+  // const { title, value, type, category } = response.body;
+
+  // const transactionRepository = new TransactionsRepository();
+  
+});
+
+transactionsRouter.delete('/:id', async (request, response) => {
+  // TODO
+});
+
+transactionsRouter.post('/import', async (request, response) => {
+  // TODO
+});
+
+export default transactionsRouter;
